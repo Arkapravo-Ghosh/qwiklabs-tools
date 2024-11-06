65fd879750bb2c33bd87da4db738e311d191ff5e
Arkapravo-Ghosh <arkapravoghosh99@gmail.com>
2024-11-07 00:16:53 +0530

    feat: added better progress output and changed assignments configuration

--- src/progress.ts
+++ src/progress.ts
@@ -1,25 +1,36 @@
 import data from "./assets/profiles_scraped_data.json";
 import assignments from "./assets/assignments.json";
 
-const completed_all = data.filter((user) => user.completed_assignments_count === assignments.length);
+const completed_all = data.filter((user) => user.completed_assignments_count === assignments.assignments.length && user.arcade_badge_progress === true);
 console.log("Number of users who have completed all badges:", completed_all.length + "\n");
 
-const completed_some = data.filter((user) => user.completed_assignments_count > 0 && user.completed_assignments_count < assignments.length);
+const completed_some = data.filter((user) => user.completed_assignments_count > 0 && user.completed_assignments_count < assignments.assignments.length);
 
 console.log("Number of users who have completed some badges:", completed_some.length + "\n");
 
-const completed_none = data.filter((user) => user.completed_assignments_count === 0);
+const completed_none = data.filter((user) => user.completed_assignments_count === 0 && user.arcade_badge_progress === false);
 console.log("Number of users who have completed no badges:", completed_none.length + "\n");
 
 const completed_assignments_count = data.map((user) => user.completed_assignments_count);
 
-for (let index = 0; index <= assignments.length; index++) {
+for (let index = 0; index <= assignments.assignments.length; index++) {
   const count = completed_assignments_count.filter((num) => num === index).length;
   console.log(`Number of users who have completed ${index} badges:`, count);
 
-  if (index === assignments.length) {
-    const users = data.filter((user) => user.completed_assignments_count === 16);
+  if (index === assignments.assignments.length) {
+    const users = data.filter((user) => user.completed_assignments_count === assignments.assignments.length && user.arcade_badge_progress === true);
     console.log("\nUsers who have completed all badges:");
     users.forEach((user) => console.log(user.name));
+
+    const non_arcade = data.filter((user) => user.completed_assignments_count === assignments.assignments.length && user.arcade_badge_progress === false);
+    non_arcade.length > 0 && console.log("\nUsers who have completed all badges but not the Arcade badge:");
+    non_arcade.forEach((user) => console.log(user.name));
   };
 };
+
+console.log("\nNumber of users who have completed the Arcade badge:", data.filter((user) => user.arcade_badge_progress === true).length);
+
+assignments.assignments.map((assignment) => {
+  const count = data.filter((user) => user.completed_assignments.includes(assignment)).length;
+  console.log(`Number of users who have completed "${assignment}":`, count);
+});
--- src/scrape.ts
+++ src/scrape.ts
@@ -4,7 +4,8 @@ import fs from "fs";
 import data from "./assets/profiles.json";
 import assignments from "./assets/assignments.json";
 
-const typedAssignments: string[] = assignments;
+const typedAssignments: string[] = assignments.assignments;
+const arcadeAssignments: string[] = assignments.arcade_assignments;
 
 const scrapeProfile = async (url: string): Promise<string[]> => {
   const response = await axios.get(url);
@@ -24,6 +25,7 @@ interface ProfileData {
   email: string;
   profile_url: string;
   badges_count: number;
+  arcade_badge_progress: boolean;
   incomplete_assignments_count: number;
   completed_assignments_count: number;
   badges: string[];
@@ -36,15 +38,24 @@ const main = async (): Promise<void> => {
   const new_data: ProfileData[] = [];
   for (const profile of data) {
     const badges: string[] = await scrapeProfile(profile["Google Cloud Skills Boost Profile URL"]);
+    const getArcadeProgress = (badges: string[]): boolean => {
+      for (const arcadeBadge of arcadeAssignments) {
+        if (badges.includes(arcadeBadge)) {
+          return true;
+        };
+      };
+      return false;
+    };
     const scraped_data: ProfileData = {
       name: profile["User Name"],
       email: profile["User Email"],
       profile_url: profile["Google Cloud Skills Boost Profile URL"],
       badges_count: badges.length,
-      incomplete_assignments_count: assignments.length - badges.length,
+      arcade_badge_progress: getArcadeProgress(badges),
+      incomplete_assignments_count: typedAssignments.length - badges.length,
       completed_assignments_count: badges.filter((badge) => typedAssignments.includes(badge)).length,
       badges: badges,
-      incomplete_assignments: assignments.filter((assignment) => !badges.includes(assignment)),
+      incomplete_assignments: typedAssignments.filter((assignment) => !badges.includes(assignment)),
       completed_assignments: badges.filter((badge) => typedAssignments.includes(badge)),
     };
     console.log(scraped_data);
